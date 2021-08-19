9dc76ee2f5dfa1a97b2a47e984e31e94ebe9c5b5
youredith <83350573+youredith@users.noreply.github.com>
2021-08-19 23:37:10 +0900

    owner problem

---
+++ src/controllers/videoController.js
@@ -0,0 +1,87 @@
+import Video from "../models/Video";
+import User from "../models/User";
+
+export const home = async(req, res) => { 
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = async(req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id).populate("owner");    
+    if (!video) {
+        return res.status(404).render("404", { pageTitle: "Video not found."});
+    }
+    console.log(video); 
+    return res.render("watch", { pageTitle: video.title, video });
+};
+export const getEdit = async (req, res) => {
+    const { id } = req.params;    
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.status(404).render("404", { pageTitle: "Video not found."});
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });    
+};
+export const postEdit = async(req, res) => {
+    const { id } = req.params;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.status(404).render("404", { pageTitle: "Video not found."});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });    
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video"});
+};
+export const postUpload = async (req, res) => {
+    const {
+        user:{ _id }
+    } = req.session;    
+    const { path: fileUrl } = req.file;
+    const { title, description, hashtags } = req.body;
+    try {
+        const newVideo = await Video.create({
+            title,
+            description,     
+            fileUrl,
+            owner: _id,
+            hashtags: Video.formatHashtags(hashtags),
+        });        
+        const user = await User.findById(_id);        
+        user.videos.push(newVideo._id);        
+        user.save();        
+        return res.redirect("/");
+    } catch(error) {
+        console.log(error);
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,            
+        });
+    }    
+}; 
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(`${keyword}$`, "i"),
+            },
+        });
+    }
+    return res.render("search", { pageTitle: "Search", videos });
+};
---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import "./db";
+import "./models/Video";
+import "./models/User";
+import app from "./server";
+
+const PORT = 4000;
+const handleListening = () => console.log(`Server Listening on http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
